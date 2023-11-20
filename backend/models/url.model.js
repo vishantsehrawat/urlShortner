c97b2d344d628f9c07fbf7741eb800c63c585587
vishant <vishant.96.sehrawat@gmail.com>
2023-11-20 16:17:28 +0530

    made changes to shema to accomodate expiration time

--- backend/models/url.model.js
+++ backend/models/url.model.js
@@ -1,19 +1,24 @@
 const mongoose = require("mongoose");
 const shortid = require("shortid");
 
-const urlSchema = new mongoose.Schema({
-  longUrl: { type: String, required: true },
-  shortUrl: {
-    type: String,
-    required: true,
-    unique: true,
-    default: shortid.generate(), // can use if we dont want ot generate it during the saving
+const urlSchema = new mongoose.Schema(
+  {
+    longUrl: { type: String, required: true },
+    shortUrl: {
+      type: String,
+      required: true,
+      unique: true,
+      default: shortid.generate(), // can use if we dont want ot generate it during the saving
+    },
+    visits: { type: Number, required: true, default: 0 },
+    createdAt: { type: Date, default: Date.now },
+    // expiry: { type: Date, default: Date.now, expires: 10 }, // 24 hours =  (86400 seconds) //^ we can also ask the user for specfic date of expiration
+    // expiry: { type: Date, default: Date.now() + 24 * 60 * 60 * 1000 }, // Expiry set to 24 hours from creation
+    expiry: { type: Date, default: Date.now, expires: "10s" }, // Set expiration to 10 seconds
   },
-  visits: { type: Number, required: true, default: 0 },
-  createdAt: { type: Date, default: Date.now },
-  expiry: { type: Date, default: Date.now, expires: 86400 }, // expires in 24 hours (86400 seconds) //^ we can also ask the user for specfic date of expiration
-});
-urlSchema.index({ expiry: 1 }, { expireAfterSeconds: 0 });
+  { versionKey: false, timestamps: true }
+);
+urlSchema.index({ expiry: 1 }, { expireAfterSeconds: 10 });
 
 const UrlModel = mongoose.model("url", urlSchema);
 
